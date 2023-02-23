759a6b547d2fb518f877e50e2e304811f73fce90
Brandon Castro <castroyou999@gmail.com>
2023-02-23 12:09:42 -0500

    commit tree 45b5caa67bf0c715dab2b524857338d123da7bbd

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,46 @@
 # your code here
+class Stack
+    attr_reader :stack, :limit
+
+    def initialize(limit = nil )
+        @stack =  []
+        @limit = limit
+    end
+
+    def pop
+        stack.pop
+    end
+
+    def push(value)
+        if !full?
+            stack.push(value)
+        else
+            raise Exception.new('Stack Overflow')
+        end
+    end
+
+    def peek
+        stack.last
+        # stack[stack.length - 1]
+    end
+
+    def size
+        stack.size
+    end
+
+    def empty?
+        stack.size == 0 
+    end
+
+    def full?
+        stack.size == limit 
+    end
+
+    def search(value)
+        stack.each_with_index do |v,i|
+            return size - 1 - i if v == value
+        end
+        return -1
+    end
+
+end
